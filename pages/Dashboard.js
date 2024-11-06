fb5067216f93b0257fc0b803ffa2753795568bb7
KotrikeAmruthaVarsha <145061730+varsha2132@users.noreply.github.com>
2024-11-06 19:00:21 +0530

    Add files via upload

---
+++ pages/Dashboard.js
@@ -0,0 +1,11 @@
+// src/pages/Dashboard.js
+import React from 'react';
+import DashboardContent from '../components/DashboardContent';
+
+const Dashboard = () => (
+  <div>
+    <DashboardContent />
+  </div>
+);
+
+export default Dashboard;
---
+++ pages/Settings.js
@@ -0,0 +1,12 @@
+// src/pages/Settings.js
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Settings = () => (
+  <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: '64px' }}>
+    <Typography variant="h4">Settings</Typography>
+    {/* Add your settings components here */}
+  </Box>
+);
+
+export default Settings;
